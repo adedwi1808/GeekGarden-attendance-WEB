8c1a5ea01d14bc96e1c43ab560ffa5327e8974d5
adedwi1808 <adedwip1808@gmail.com>
2022-07-16 23:30:01 +0700

    Revert "mengcustom middleware jwt agar sesuai dengan guard yang baru dan beberapa penyesuaian pada model izin dan pegawai untuk dapat berelasi"
    This reverts commit 63741dd920c55048a4fd4bb4166cbc46c5f711f7.

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
+
+Route::post('/upload-attendance-image/{id}', [\App\Http\Controllers\Api\PegawaiController::class, 'uploadbuktiabsensi']);
+Route::post('/upload-complete-attendance-image/{id}', [\App\Http\Controllers\Api\PegawaiController::class, 'uploadCompleteAttendanceImage']);
 Route::post('/upload-mading', [\App\Http\Controllers\Api\MadingController::class, 'uploadMading']);
+
 
 //Pegawai
 Route::post('/register-pegawai', [AuthPegawaiController::class, 'pegawairegister'])->name('pegawairegister');
@@ -29,16 +34,16 @@ Route::group(['middleware' => ['assign.guard:pegawai-api', 'jwt.verify']], funct
     Route::post('/me-pegawai', [AuthPegawaiController::class, 'me'])->name('me');
     Route::put('/update-pegawai/{id}', [AuthPegawaiController::class, 'updatepegawai']);
     Route::post('/logout-pegawai', [AuthPegawaiController::class, 'logout']);
+    //Other
+    Route::get('/madings', [\App\Http\Controllers\Api\MadingController::class, 'selectAllMading']);
+    Route::post('/foto-pegawai/{id}', [AuthPegawaiController::class, 'editfotoprofile']);
+    Route::get('/cek-absensi', [\App\Http\Controllers\Api\PegawaiController::class, 'checkabsensi']);
 
     //Attendance
     Route::post('/absensi-hadir',[\App\Http\Controllers\Api\PegawaiController::class, 'absensihadir']);
     Route::post('/upload-bukti-absensi/{id}', [\App\Http\Controllers\Api\PegawaiController::class, 'uploadbuktiabsensi']);
     Route::post('/absensi-pulang',[\App\Http\Controllers\Api\PegawaiController::class, 'absensipulang']);
 
-    //Other
-    Route::get('/madings', [\App\Http\Controllers\Api\MadingController::class, 'selectAllMading']);
-    Route::post('/foto-pegawai/{id}', [AuthPegawaiController::class, 'editfotoprofile']);
-    Route::get('/cek-absensi', [\App\Http\Controllers\Api\PegawaiController::class, 'checkabsensi']);
 });
 
 
