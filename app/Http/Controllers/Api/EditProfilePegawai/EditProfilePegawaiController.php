23f400d88695728a8bc4c89efdbdf287c776681d
adedwi1808 <adedwip1808@gmail.com>
2022-08-14 12:59:18 +0700

    FIX: fix bug update password pegawai

--- app/Http/Controllers/Api/EditProfilePegawai/EditProfilePegawaiController.php
+++ app/Http/Controllers/Api/EditProfilePegawai/EditProfilePegawaiController.php
@@ -5,14 +5,31 @@ namespace App\Http\Controllers\Api\EditProfilePegawai;
 use App\Http\Controllers\Controller;
 use App\Models\Pegawai;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class EditProfilePegawaiController extends Controller
 {
     public function updatepegawai(Request $request, $id)
     {
         $pegawai = Pegawai::where('id_pegawai', $id)->first();
+
+        $validasi = Validator::make($request->all(), [
+            'password' => 'required|min:6'
+        ]);
+
         if($pegawai){
-            $pegawai->update($request->all());
+            if ($validasi->fails()){
+                $pegawai->update([
+                    'email'=> $request->email,
+                    'nomor_hp' => $request->nomor_hp
+                ]);
+            }else{
+                $pegawai->update([
+                    'email'=> $request->email,
+                    'nomor_hp' => $request->nomor_hp,
+                    'password' => bcrypt($request->password)
+                ]);
+            }
             return $this->success($pegawai);
         }
         return $this->error("Error User tidak ditemukan");
