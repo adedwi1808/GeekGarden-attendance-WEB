6f35c4da91beeb5b920a2dac64864d9e0441d6ed
adedwi1808 <adedwip1808@gmail.com>
2023-05-21 18:08:56 +0700

    fix: config filesystem  public

--- config/filesystems.php
+++ config/filesystems.php
@@ -33,15 +33,25 @@ return [
         'local' => [
             'driver' => 'local',
             'root' => storage_path('app'),
+            'visibility' => 'public',
             'throw' => false,
         ],
 
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
-            'throw' => false,
+            'permissions' => [
+                'file' => [
+                    'public' => 0644,
+                    'private' => 0600,
+                ],
+                'dir' => [
+                    'public' => 0755,
+                    'private' => 0700,
+                ],
+            ],
         ],
 
         's3' => [
