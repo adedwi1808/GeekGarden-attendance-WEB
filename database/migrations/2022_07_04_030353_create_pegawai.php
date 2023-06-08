ccade692a52f1b86a466caa0e5bbe7e43f6d73cd
adedwi1808 <adedwip1808@gmail.com>
2023-06-08 08:51:57 +0700

    no message

--- database/migrations/2022_07_04_030353_create_pegawai.php
+++ database/migrations/2022_07_04_030353_create_pegawai.php
@@ -1,38 +1,38 @@
 <?php
-
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
-
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('pegawai', function (Blueprint $table) {
-            $table->id('id_pegawai');
-            $table->string('nama');
-            $table->enum('jenis_kelamin',["Laki-laki", "Perempuan"]);
-            $table->string('nomor_hp')->unique();
-            $table->string('email')->unique();
-            $table->string('jabatan');
-            $table->string('foto_profile')->nullable();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-        });
-    }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('pegawai');
-    }
-};
+//
+//use Illuminate\Database\Migrations\Migration;
+//use Illuminate\Database\Schema\Blueprint;
+//use Illuminate\Support\Facades\Schema;
+//
+//return new class extends Migration
+//{
+//    /**
+//     * Run the migrations.
+//     *
+//     * @return void
+//     */
+//    public function up()
+//    {
+//        Schema::create('pegawai', function (Blueprint $table) {
+//            $table->id('id_pegawai');
+//            $table->string('nama');
+//            $table->enum('jenis_kelamin',["Laki-laki", "Perempuan"]);
+//            $table->string('nomor_hp')->unique();
+//            $table->string('email')->unique();
+//            $table->string('jabatan');
+//            $table->string('foto_profile')->nullable();
+//            $table->timestamp('email_verified_at')->nullable();
+//            $table->string('password');
+//        });
+//    }
+//
+//    /**
+//     * Reverse the migrations.
+//     *
+//     * @return void
+//     */
+//    public function down()
+//    {
+//        Schema::dropIfExists('pegawai');
+//    }
+//};
